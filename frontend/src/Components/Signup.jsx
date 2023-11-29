d5a0c056e7120b624151aba38ed577d695662731
Guillaume <guizanko@hotmail.fr>
2023-11-29 16:10:08 +0100

    ok

--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -7,66 +6,65 @@ function Sign() {
     <div>
       <h1 className="bg-dark text-xl text-white">Sign Up Page</h1>
 
-      <div class="bg-grey-lighter min-h-screen flex flex-col">
-        <div class="container glass max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-          
-          <div classname='bg-lime-200 px-6 py-8 rounded shadow-md text-black w-full'>
-            <h1 class="mb-8 text-3xl text-center">Sign up</h1>
+      <div className="bg-grey-lighter min-h-screen flex flex-col">
+        <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2 relative">
+          <div className='glass px-6 py-8 rounded shadow-md text-black w-full' style={{ backdropFilter: 'blur(5px)' }}>
+            <h1 className="mb-8 text-3xl text-center">Sign up</h1>
             <input
               type="text"
-                  class=" glass block border border-grey-light w-full p-3 rounded mb-4"
+              className="glass block border border-grey-light w-full p-3 rounded mb-4"
               name="fullname"
               placeholder="Full Name"
             />
 
             <input
               type="text"
-              class="glass block border border-grey-light w-full p-3 rounded mb-4"
+              className="glass block border border-grey-light w-full p-3 rounded mb-4"
               name="email"
               placeholder="Email"
             />
 
             <input
               type="password"
-              class=" glass block border border-grey-light w-full p-3 rounded mb-4"
+              className="glass block border border-grey-light w-full p-3 rounded mb-4"
               name="password"
               placeholder="Password"
             />
             <input
               type="password"
-              class="glass block border border-grey-light w-full p-3 rounded mb-4"
+              className="glass block border border-grey-light w-full p-3 rounded mb-4"
               name="confirm_password"
               placeholder="Confirm Password"
             />
 
             <button
               type="submit"
-              class="glass bg-lime-100 block border border-grey-light w-full p-3 rounded mb-4 w-full text-darkq"
+              className="glass bg-lime-100 block border border-grey-light w-full p-3 rounded mb-4 w-full text-darkq"
             >
               Create Account
             </button>
 
-            <div class="text-center text-sm text-grey-dark mt-4">
-              By signing up, you agree to the 
+            <div className="text-center text-sm text-grey-dark mt-4">
+              By signing up, you agree to the
               <a
-                class="no-underline border-b border-grey-dark text-grey-dark"
+                className="no-underline border-b border-grey-dark text-grey-dark"
                 href="#"
               >
-                 Terms of Service
+                Terms of Service
               </a>{" "}
               and
               <a
-                class="no-underline border-b border-grey-dark text-grey-dark"
+                className="no-underline border-b border-grey-dark text-grey-dark"
                 href="#"
               >
                 Privacy Policy
               </a>
             </div>
           </div>
-          <div class="text-white mt-6">
+          <div className="text-white mt-6">
             Already have an account?
             <a
-              class="no-underline border-b border-blue text-white"
+              className="no-underline border-b border-blue text-white"
               href="../login/"
             >
               Log in
@@ -76,7 +74,6 @@ function Sign() {
         </div>
       </div>
     </div>
-    
   );
 }
 
