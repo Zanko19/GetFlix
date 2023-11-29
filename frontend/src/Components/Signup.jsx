9d8c6bf8a011669a6177468cd1c518acba162b4b
Guillaume <guizanko@hotmail.fr>
2023-11-29 16:36:23 +0100

    see

--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -8,8 +8,8 @@ function Sign() {
 
       <div className="bg-grey-lighter min-h-screen flex flex-col">
         <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2 relative">
-          <div className='glass px-6 py-8 rounded shadow-md text-black w-full' style={{ backdropFilter: 'blur(5px)' }}>
-            <h1 className="mb-8 text-3xl text-center">Sign up</h1>
+          <div className='glass px-6 py-8 rounded shadow-md text-dark w-full'>
+            <h1 className="mb-8 text-3xl text-center text-white">Sign up</h1>
             <input
               type="text"
               className="glass block border border-grey-light w-full p-3 rounded mb-4"
@@ -39,7 +39,7 @@ function Sign() {
 
             <button
               type="submit"
-              className="glass bg-lime-100 block border border-grey-light w-full p-3 rounded mb-4 w-full text-darkq"
+              className="glass bg-lime-100 block border border-grey-light w-full p-3 rounded mb-4"
             >
               Create Account
             </button>
@@ -50,26 +50,26 @@ function Sign() {
                 className="no-underline border-b border-grey-dark text-grey-dark"
                 href="#"
               >
-                Terms of Service
+                <p class="underline">Terms of Service</p>
               </a>{" "}
               and
               <a
                 className="no-underline border-b border-grey-dark text-grey-dark"
                 href="#"
               >
-                Privacy Policy
+                <p class="underline">Privacy Policy</p>
               </a>
             </div>
           </div>
           <div className="text-white mt-6">
-            Already have an account?
+            <p>Already have an account?</p>
             <a
               className="no-underline border-b border-blue text-white"
               href="../login/"
             >
-              Log in
-            </a>
-            .
+              <p class="text-center underline">Log in
+            </p></a>
+            
           </div>
         </div>
       </div>
