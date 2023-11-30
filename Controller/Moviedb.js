dcb7ed668848fb20b51ec1565fc291f6c22cbe13
Julia Michel T. <juliamicheltex@gmail.com>
2023-11-30 12:15:16 +0100

    data take

--- Controller/Moviedb.js
+++ Controller/Moviedb.js
@@ -1,18 +1,42 @@
-const MovieDB = require("node-themoviedb");
+//Discovermovies + Genres
+const genreUrl = "https://api.themoviedb.org/3/genre/movie/list?language=en";
 
-import MovieDB from "node-themoviedb";
-const mdb = new MovieDB("717c65444a1b44b426f908cdbbd4c091", options);
+const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+  movieName
+)}&include_adult=false&language=en-US&page=1&sort_by=popularity.desc`;
 
-(async () => {
-  try {
-    const args = {
-      pathParameters: {
-        movie_id: 384018,
-      },
-    };
-    const movie = await mdb.movie.getDetails(args);
-    console.log(movie);
-  } catch (error) {
-    console.error(error);
-  }
-})();
+//FECTH DATA
+Promise.all([
+  fetch(genreUrl, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDM0MDVjZTkwYWQ3YWJiY2M2NmVjZGQ4MzQzODAyMCIsInN1YiI6IjY1Njg0NjQzOWFmZmMwMDBlYmNmMzU4NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m4dPEXx0u6ZzRO0DzSsS5h-9bl8n5RvrpRdhvtNhCjI",
+    },
+  }),
+  fetch(searchUrl, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDM0MDVjZTkwYWQ3YWJiY2M2NmVjZGQ4MzQzODAyMCIsInN1YiI6IjY1Njg0NjQzOWFmZmMwMDBlYmNmMzU4NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m4dPEXx0u6ZzRO0DzSsS5h-9bl8n5RvrpRdhvtNhCjI",
+    },
+  }),
+]);
+
+//LA DATA
+releaseDateElement.textContent = `Release Date: ${movie.release_date}`;
+overviewElement.textContent = `Overview: ${movie.overview}`;
+voteAverageElement.textContent = `Vote Average: ${movie.vote_average}`;
+posterElement.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+posterElement.alt = `${movie.title} Poster`;
+
+// Attention extraire les genres des id
+const genreNames = movie.genre_ids.map((genreId) => {
+  const genre = genres.find((genre) => genre.id === genreId);
+  return genre ? genre.name : "Unknown";
+});
+
+//les genres extraits
+categoriesElement.textContent = `Categories: ${genreNames.join(", ")}`;
