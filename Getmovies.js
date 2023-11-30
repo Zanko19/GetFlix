003e92ce941a05eedd0d8f6bb5b7f5c72f0f95a2
Julia Michel T. <juliamicheltex@gmail.com>
2023-11-30 11:07:08 +0100

    ignore files

--- Getmovies.js
+++ Getmovies.js
@@ -1,13 +1,11 @@
 const fetch = require("node-fetch");
 
-const url =
-  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+const url = "https://api.themoviedb.org/3/configuration";
 const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTdjNjU0NDRhMWI0NGI0MjZmOTA4Y2RiYmQ0YzA5MSIsInN1YiI6IjY1Njg0NjQzOWFmZmMwMDBlYmNmMzU4NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ezU35302lrY0T_rG3knYsb22MpvDzca1ZwXNkt-RKPM",
+    Authorization: "Bearer cbdc9f84e92b03d99f1fdff697fa2ad7",
   },
 };
 
