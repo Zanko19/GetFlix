592dccba7ec677f6ffc294dc17a5a9e8bcdf4b1b
Guillaume <guizanko@hotmail.fr>
2023-12-07 16:27:15 +0100

    loginsignup

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,25 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import screen from "./img/screen.jpeg";
+
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   return (
-    <div className="flex">
-    
-      <div className="w-1/2">
+    <div className="w-[100%] h-[100vh]  flex items-center justify-center overflow-hidden">
+      <div className="w-[80%] h-[80%] flex flex-row items-center  rounded-3xl">
         <img
-          src="chemin/vers/votre/image.jpg" 
-          alt="Description de l'image"
-          className="w-full h-full object-cover"
+          src={screen}
+          alt="screen"
+          className="w-2/3 hidden lg:flex h-full object-cover rounded-l-3xl xs:rounded-l-3xl"
         />
-      </div>
-    <div className="glass h-[100vh] px-3 sm:px-1 flex items-center justify-center absolute">
-      <div className="glass w-1/2 sm:w-2/3 lg:2/3 px-6 text-white z-50 py-4 rounded-lg">
-        <div className="w-full flex justify-center text-[#b4d429] text-xl mb:2 md:mb-5">
+      <div className="w-1/3 h-full flex-auto px-6 lglass text-white py-4 rounded-r-3xl rounded-l-3xl lg:rounded-l-none ">
+        <div className="w-full flex justify-center text-[#b4d429] text-3xl font-bold ">
           Log in
         </div>
         <div className="mb-6">
           <label
             htmlFor="email"
-            className="block mb-2 text-xs font-medium text-[#b4d429]"
+            className="block mb-2 text-xs font-medium text-white"
           >
             Your email
           </label>
@@ -32,35 +37,48 @@ function Login() {
           />
         </div>
         <div className="mb-6">
-          <label
-            htmlFor="password"
-            className="block mb-2 text-xs font-medium text-[#b4d429]"
-          >
-            Your password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="bg-[#153635] border text-gray-900 text-xs rounded-lg focus:ring block w-full p-1.5 md:p-2.5 "
-            placeholder="XXXXXXX"
-            required
-          />
+      <label
+        htmlFor="password"
+        className="block mb-2 text-xs font-medium text-white"
+      >
+        Your Password
+      </label>
+      <div className="relative">
+        <input
+          type={showPassword ? "text" : "password"}
+          id="password"
+          className="lglass border border-gray-300 text-white text-xs rounded-lg focus:ring ring-green-100 block w-full p-1.5 md:p-2.5"
+          placeholder="XXXXXXX"
+          required
+        />
+        <button
+          type="button"
+          onClick={togglePasswordVisibility}
+          className="absolute top-1/2 right-2 transform -translate-y-1/2"
+        >
+          {showPassword ? (
+            <FaEyeSlash className="h-5 w-5 text-[#b4d429]" />
+          ) : (
+            <FaEye className="h-5 w-5 text-[#b4d429]" />
+          )}
+        </button>
         </div>
         <div className="flex flex-row justify-between">
-          <div className="text-[#b4d429] text-sm md:text-md justify-between">
+          <div className="text-[#b4d429] text-sm md:text-md hover:scale-105 hover:text-white">
             <a href="">Forgot Password?</a>
           </div>
-          <div className="text-[#b4d429] text-sm md:text-md">
+          <div className="text-[#b4d429] text-sm md:text-md ">
             Don't have an account ?{" "}
-            <a className="underline" href="../signup">
+            <a className="underline hover:scale-105 hover:text-white" href="../signup">
               Signup here
             </a>
           </div>
         </div>
-        <div className="mt-2 md:mt-4 sm:w-2/3 w-60 mx-auto flex items-center justify-center text-sm md:text-xl bg-[#60701a] py-2 rounded-md text-[#b4d429]">
+        <div className="fancy w-1/2 sm:w-full mt-2 md:mt-4 w-60 mx-auto flex items-center justify-center text-xl md:text-xl bg-[#60701a] py-2 rounded-md text-[#b4d429] border border-[#b4d429] hover:scale-105 group-hover:animate-shine focus:ring">
           <a href="#">Login</a>
         </div>
       </div>
+    </div>
     </div>
     </div>
   );
