94d6e4be7102b5d3b30df4c4d178fb42d0b4e56c
A3lequenne <clementlequenne1@gmail.com>
2023-12-13 15:26:24 +0100

    test deploy

--- backend/Routes/MovieRoutes.js
+++ backend/Routes/MovieRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const MovieController = require("../Controller/MovieDbFilter");
+const MovieController = require("../Controller/MovieController");
 // const userController = require("../controllers/UserController");
 
 const router = express.Router();
