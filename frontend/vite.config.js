c71de6613fa3902233a43e42acdfa12b7bc51498
Julia Michel T. <juliamicheltex@gmail.com>
2023-12-15 21:46:04 +0100

    deployed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/getflixProject/",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
+  // build: {
+  //   outDir: "dist",
+  // },
 });
