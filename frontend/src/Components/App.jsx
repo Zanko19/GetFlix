91532b95712b6f9486d5854961a2b25e007f2ab6
Julia Michel T. <juliamicheltex@gmail.com>
2023-12-15 21:17:00 +0100

    push

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Password from "./Forgot";
 import Movie from "./Onemovie";
 import Sign from "./Signup";
