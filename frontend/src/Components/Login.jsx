df962c74c834a984c829838f2b09ff7acb7f0707
Julia Michel T. <juliamicheltex@gmail.com>
2023-12-17 17:54:25 +0100

    login connected

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,14 +1,55 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom"; // Import useNavigate
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import screen from "../img/screen.jpeg";
 
 function Login() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
   const [showPassword, setShowPassword] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://157.230.127.29/users/login", {
+        method: "POST",
+        body: JSON.stringify(formData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log("Response Status:", response.status);
+
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log(responseData.message); // Log success message
+        navigate("/");
+      } else {
+        const errorResponse = await response.json();
+        console.error("Error during login:", errorResponse.message);
+      }
+    } catch (error) {
+      console.error("Error during login:", error.message);
+    }
+  };
+
   return (
     <div className="w-[100%] h-[100vh]  flex items-center justify-center overflow-hidden">
       <div className="w-[80%] h-[80%] flex flex-row items-center justify-center rounded-3xl">
@@ -21,64 +62,72 @@ function Login() {
           <div className="w-full flex justify-center text-white text-3xl font-bold mb-10 ">
             Log in
           </div>
-          <div className="mb-6 ">
-            <label
-              htmlFor="email"
-              className="block mb-2 text-xs font-medium text-white "
-            >
-              Your email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="lglass border border-gray-300 text-white text-xs rounded-lg focus:ring ring-green-100 block w-full p-1.5 md:p-2.5"
-              placeholder="sarahwillis@hotmail.com"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="password"
-              className="block mb-2 text-xs font-medium text-white"
-            >
-              Your Password
-            </label>
-            <div className="relative">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-6 ">
+              <label
+                htmlFor="username"
+                className="block mb-2 text-xs font-medium text-white "
+              >
+                Your username
+              </label>
               <input
-                type={showPassword ? "text" : "password"}
-                id="password"
-                className="lglass border border-gray-300 text-white text-xs rounded-lg focus:ring ring-green-100 block w-full p-1.5 md:p-2.5 mb-2"
-                placeholder="XXXXXXX"
+                type="text"
+                id="username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                className="lglass border border-gray-300 text-white text-xs rounded-lg focus:ring ring-green-100 block w-full p-1.5 md:p-2.5"
+                placeholder="sarahwillis"
                 required
               />
-              <button
-                type="button"
-                onClick={togglePasswordVisibility}
-                className="absolute top-1/2 right-2 transform -translate-y-1/2"
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="block mb-2 text-xs font-medium text-white"
               >
-                {showPassword ? (
-                  <FaEyeSlash className="h-5 w-5 text-[#b4d429]" />
-                ) : (
-                  <FaEye className="h-5 w-5 text-[#b4d429]" />
-                )}
-              </button>
+                Your Password
+              </label>
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  className="lglass border border-gray-300 text-white text-xs rounded-lg focus:ring ring-green-100 block w-full p-1.5 md:p-2.5 mb-2"
+                  placeholder="XXXXXXX"
+                  required
+                />
+                <button
+                  type="button"
+                  onClick={togglePasswordVisibility}
+                  className="absolute top-1/2 right-2 transform -translate-y-1/2"
+                >
+                  {showPassword ? (
+                    <FaEyeSlash className="h-5 w-5 text-[#b4d429]" />
+                  ) : (
+                    <FaEye className="h-5 w-5 text-[#b4d429]" />
+                  )}
+                </button>
+              </div>
             </div>
             <div className="flex  flex-row justify-between">
               <div className="mt-2 text-[#b4d429] text-sm hover:scale-105 hover:text-white mb-10">
-              <Link to="/forgot">Forgot Password</Link>
+                <Link to="/forgot">Forgot Password</Link>
               </div>
               <div className="mt-2 text-[#b4d429] text-sm md:text-md hover:scale-105 hover:text-white">
-              <Link to="/signup">Signup here</Link>
+                <Link to="/signup">Signup here</Link>
               </div>
             </div>
 
-            <a href="#">
-              {" "}
-              <div className="fancy w-1/2 flex mt-2 md:w-60 md:mb-4 w-40 mx-auto flex items-center justify-center text-xl md:text-xl bg-greeny/30 py-2 rounded-md text-white border border-[#b4d429] hover:scale-105 group-hover:animate-shine">
-                Login
-              </div>
-            </a>
-          </div>
+            <button
+              type="submit"
+              className="fancy w-1/2 flex mt-2 md:w-60 md:mb-4 w-40 mx-auto flex items-center justify-center text-xl md:text-xl bg-greeny/30 py-2 rounded-md text-white border border-[#b4d429] hover:scale-105 group-hover:animate-shine"
+            >
+              Login
+            </button>
+          </form>
         </div>
       </div>
     </div>
