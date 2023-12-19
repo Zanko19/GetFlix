d13c40d64bd9d703a8a903abe8f9e01b0bbf4378
Clément Lequenne <clementlequenne1@gmail.com>
2023-12-19 16:10:10 +0100

    added readme

--- backend/Model/AccesMovieDb.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  apiKey: "403405ce90ad7abbcc66ecdd83438020",
-  TokenKey:
-    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDM0MDVjZTkwYWQ3YWJiY2M2NmVjZGQ4MzQzODAyMCIsInN1YiI6IjY1Njg0NjQzOWFmZmMwMDBlYmNmMzU4NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m4dPEXx0u6ZzRO0DzSsS5h-9bl8n5RvrpRdhvtNhCjI",
-};
