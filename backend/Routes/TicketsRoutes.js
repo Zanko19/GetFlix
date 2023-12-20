6ab85f47583346a898e7754f6bf35fb5973c8d8b
A3lequenne <clementlequenne1@gmail.com>
2023-12-20 12:02:32 +0100

    merging

--- backend/Routes/TicketsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ticketsController = require("../Controller/TicketsController");
-const authenticateUser = require("../Middleware/Auth");
-
-router.post("/buy-tickets", authenticateUser, ticketsController.buyTickets);
-
-module.exports = router;
